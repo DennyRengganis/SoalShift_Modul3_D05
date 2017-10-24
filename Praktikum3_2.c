394695b4702f4a7bbd6dc9801613f2e62bf3223a
Unknown <drdennyr1245@gmail.com>
2017-10-25 04:55:10 +0700

    nomor 2 kurang fungsi utama

--- Praktikum3_2.c
+++ Praktikum3_2.c
@@ -7,8 +7,8 @@
 pthread_t p1;
 pthread_t p2;
 int scorep1=0,scorep2=0,mines[2][17];
-char* player1;
-char* player2;
+char player1[100];
+char player2[100];
 int status=0,flag1=0,flag2=0;
 
 void point(){
@@ -18,35 +18,61 @@ printf("Point dari %s adalah : %d\n",player2,scorep2);
 
 void* turnp1(void *arg){
 	while(flag1<10){
-	while(status==1){
+		while(status==1){
 
+		}
+		if(status==999){
+		return NULL;
+		}
+		flag1++;
+		printf("flag1 : %d\n",flag1);
+		status=1;
 	}
-	flag1++;
-	printf("flag1 : %d\n",flag1);
-	status=1;
+	printf("Permainan Selesai\n");
+	point();
+	if(scorep1>scorep2){
+	printf("Pemain pertama %s menang\n",player1);
 	}
+	else if(scorep2>scorep1){
+	printf("Pemain kedua %s menang\n",player2);
+	}
+	else{
+	printf("Permainan seri\n");
+	}
+	status=999;
 	return NULL;
 }
 
 void* turnp2(void *arg){
 	while(flag2<10){
-	while(status==0){
+		while(status==0){
 
+		}
+		if(status==999){
+		return NULL;
+		}
+		flag2++;
+		printf("flag2 : %d\n",flag2);
+		status=0;
 	}
-	flag2++;
-	printf("flag2 : %d\n",flag2);
-	status=0;
-	}
+        printf("Permainan Selesai");
+        point();
+        if(scorep1>scorep2){
+        printf("Pemain pertama %s menang\n",player1);
+        }
+        else if(scorep2>scorep1){
+        printf("Pemain kedua %s menang\n",player2);
+        }
+        else{
+        printf("Permainan seri\n");
+        }
+        status=999;
 	return NULL;
 }
 
 int main(){
 	printf("Masukkan Nama pemain pertama dan pemain kedua  : ");
 	scanf("%s %s", player1,player2);
-//	getchar();
-//	printf("Masukkan Nama pemain kedua :" );
-//	scanf("%s", player2);
-//	getchar();
 
 	pthread_create(&(p1),NULL,&turnp1, NULL);
 	pthread_create(&(p2),NULL,&turnp2, NULL);
