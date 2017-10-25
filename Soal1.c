f6c580c9bc029e6847c76c9c795545e9ce49530e
Unknown <drdennyr1245@gmail.com>
2017-10-25 16:43:02 +0700

    Soal1.c fungsi pembeli

--- Soal1.c
+++ Soal1.c
@@ -19,6 +19,56 @@ void menupembeli(){
 	printf("Pilih : ");	
 }
 
+void pembeli(int* jmlh){
+	int jenis,jumlah,pilih,i;
+	char nsenjata[100];
+	while(1){
+		menupembeli();
+		scanf("%d",&pilih);
+		if(pilih==1){
+			for(i=0;i<6;i++){
+				printf("%s %d\n",senjata[i],jmlh[i]);
+			}
+		}
+		else if(pilih==2){
+			printf("Tuliskan nama senjata yang akan dibeli dan berapa jumlahnya ?");
+			scanf("%s %d",nsenjata,&jumlah);
+			printf("\n");
+			if(strcmp(nsenjata,"MP4A1")==0){
+				if(jmlh[0]>=jumlah) jmlh[0]-=jumlah;
+				else printf("Jumlah stok tidak cukup\n");
+			}
+			else if(strcmp(nsenjata,"PM2-V1")==0){
+				if(jmlh[1]>=jumlah) jmlh[1]-=jumlah;
+				else printf("Jumlah stok tidak cukup\n");
+			}
+                        else if(strcmp(nsenjata,"SPR-3")==0){
+                                if(jmlh[2]>=jumlah) jmlh[2]-=jumlah;
+                                else printf("Jumlah stok tidak cukup\n");
+                        }
+                        else if(strcmp(nsenjata,"SS2-V5")==0){
+                                if(jmlh[3]>=jumlah) jmlh[3]-=jumlah;
+                                else printf("Jumlah stok tidak cukup\n");
+                        }
+                        else if(strcmp(nsenjata,"SPG1-V3")==0){
+                                if(jmlh[4]>=jumlah) jmlh[4]-=jumlah;
+                                else printf("Jumlah stok tidak cukup\n");
+                        }
+                        else if(strcmp(nsenjata,"MINES")==0){
+                                if(jmlh[5]>=jumlah) jmlh[5]-=jumlah;
+                                else printf("Jumlah stok tidak cukup\n");
+                        }
+		}
+		else{
+			printf("tolong masukkan input sesuai menu");
+		}
+	}
+}
+
+void penjual(int* jmlh){
+	
+}
+
 int main(){
 	key_t key=1234;
 	int *value;
@@ -31,4 +81,13 @@ int main(){
 	printf("2. Penjual\n");
 	printf("Pilih : ");
 	scanf("%d",&peran);
+	if(peran==1){
+		pembeli(value);
+	}
+	else if(peran==2){
+		penjual(value);
+	}
+	else{
+		printf("tolong input sesuai dengan menu\n");
+	}
 }
